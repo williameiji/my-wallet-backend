a61ab3dd6671758cb0b32426e2199a737cd2c388
williameiji <william.fujimori@gmail.com>
2022-06-28 22:48:18 -0300

    feat: get information with token

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import cors from "cors";
 import joi from "joi";
 import { MongoClient, ObjectId } from "mongodb";
 import dotenv from "dotenv";
+import dayjs from "dayjs";
+import bcrypt from "bcrypt";
+import { v4 as uuid } from "uuid";
 
 dotenv.config();
 
@@ -36,30 +39,44 @@ const schemaUser = joi.object({
 	password: joi.string().pattern(new RegExp("^[a-zA-Z0-9]{3,30}$")).required(),
 });
 
+const schemaInput = joi.object({
+	value: joi.string().required(),
+	description: joi.string().required(),
+	color: joi.string(),
+});
+
 //ROTAS
 
 app.post("/signup", async (req, res) => {
-	const { error } = schemaNewUser.validate(req.body);
+	const { name, email, password, isPasswordEqual } = req.body;
+	const { error } = schemaNewUser.validate({
+		name,
+		email,
+		password,
+		isPasswordEqual,
+	});
 
 	if (error) {
-		res.sendStatus(422);
+		res.status(422).send("Dados incorretos");
 		return;
 	}
 
 	try {
 		const isUserRegistered = await db.collection("users").findOne({
-			email: req.body.email,
+			email,
 		});
 
 		if (isUserRegistered) {
-			res.sendStatus(409);
+			res.status(409).send("Usuário já cadastrado!");
 			return;
 		}
 
+		const encryptedPassaword = bcrypt.hashSync(password, 10);
+
 		await db.collection("users").insertOne({
-			name: req.body.name,
-			email: req.body.email,
-			password: req.body.password,
+			name,
+			email,
+			password: encryptedPassaword,
 		});
 
 		res.sendStatus(201);
@@ -70,7 +87,6 @@ app.post("/signup", async (req, res) => {
 
 app.post("/login", async (req, res) => {
 	const { email, password } = req.headers;
-	console.log(email, password);
 	const { error } = schemaUser.validate({
 		email: email,
 		password: password,
@@ -84,16 +100,117 @@ app.post("/login", async (req, res) => {
 	try {
 		const isUserRegistered = await db.collection("users").findOne({
 			email,
-			password,
 		});
 
-		if (!isUserRegistered) {
+		if (
+			isUserRegistered ||
+			bcrypt.compareSync(password, isUserRegistered.password)
+		) {
+			const token = uuid();
+
+			await db.collection("sessions").insertOne({
+				email: isUserRegistered.email,
+				token,
+			});
+
+			res.status(200).send(token);
+		} else {
 			res.sendStatus(401);
 			return;
 		}
-
-		res.sendStatus(200);
 	} catch (err) {
+		res.sendStatus(500);
+	}
+});
+
+app.get("/user", async (req, res) => {
+	const { authorization } = req.headers;
+	const token = authorization?.replace("Bearer ", "");
+
+	console.log(authorization);
+
+	try {
+		const getUser = await db.collection("sessions").findOne({
+			token,
+		});
+
+		if (!getUser) {
+			return res.sendStatus(404);
+		}
+
+		const user = await db.collection("users").findOne({
+			email: getUser.email,
+		});
+
+		if (!user) {
+			return res.sendStatus(404);
+		}
+
+		res.send(user.name);
+	} catch (error) {
+		res.sendStatus(500);
+	}
+});
+
+app.post("/history", async (req, res) => {
+	const { authorization } = req.headers;
+	const token = authorization?.replace("Bearer ", "");
+	const { error } = schemaInput.validate(req.body);
+
+	if (error) {
+		res.sendStatus(422);
+		return;
+	}
+
+	try {
+		const getUser = await db.collection("sessions").findOne({
+			token,
+		});
+
+		if (!getUser) return res.sendStatus(404);
+
+		const user = await db.collection("users").findOne({
+			email: getUser.email,
+		});
+
+		if (!user) return res.sendStatus(404);
+
+		await db.collection("history").insertOne({
+			email: user.email,
+			...req.body,
+			date: dayjs().format("DD-MM"),
+		});
+
+		res.sendStatus(201);
+	} catch (err) {
+		res.sendStatus(500);
+	}
+});
+
+app.get("/history", async (req, res) => {
+	const { authorization } = req.headers;
+	const token = authorization?.replace("Bearer ", "");
+
+	try {
+		const getUser = await db.collection("sessions").findOne({
+			token,
+		});
+
+		if (!getUser) return res.sendStatus(404);
+
+		const user = await db.collection("users").findOne({
+			email: getUser.email,
+		});
+
+		if (!user) return res.sendStatus(404);
+
+		const userHistory = await db
+			.collection("history")
+			.find({ email: user.email })
+			.toArray();
+
+		res.send(userHistory);
+	} catch (error) {
 		res.sendStatus(500);
 	}
 });
