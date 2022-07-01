b03d297997834349ce084510c739aa64cf50fa1d
williameiji <william.fujimori@gmail.com>
2022-07-01 16:06:54 -0300

    refactor: change uri name

--- src/databases/mongo.js
+++ src/databases/mongo.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const mongoClient = new MongoClient(process.env.MONGO_URL);
+const mongoClient = new MongoClient(process.env.MONGO_URI);
 
 await mongoClient.connect();
 const db = mongoClient.db(process.env.DATABASE_NAME);
