7e733bf42fb6d75311b045c17d12807be6483c56
williameiji <william.fujimori@gmail.com>
2022-07-01 15:43:25 -0300

    fix: signup validation

--- src/infra/validators/signupValidator.js
+++ src/infra/validators/signupValidator.js
@@ -2,9 +2,8 @@ import schemaSignup from "../schemas/schemaSignup.js";
 
 async function signupValidator(req, res, next) {
 	const newUser = req.body;
-	const { error } = schemaSignup.validate({
-		newUser,
-	});
+	console.log(newUser);
+	const { error } = schemaSignup.validate(newUser);
 
 	if (error) return res.status(422).send("Todos os campos são obrigatórios!");
 
