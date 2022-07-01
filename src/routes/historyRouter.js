3e2b5fdc54d2e03c05bb73d2ee5f36f2eee5ce79
williameiji <william.fujimori@gmail.com>
2022-07-01 17:32:29 -0300

    fix access a

--- src/routes/historyRouter.js
+++ src/routes/historyRouter.js
@@ -17,13 +17,6 @@ import editRecordValidator from "../infra/validators/editRecordValidator.js";
 
 const historyRouter = Router();
 
-historyRouter.use((req, res, next) => {
-	res.append("Access-Control-Allow-Origin", ["*"]);
-	res.append("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
-	res.append("Access-Control-Allow-Headers", "Content-Type");
-	next();
-});
-
 historyRouter.post(
 	"/history",
 	verifyToken,
