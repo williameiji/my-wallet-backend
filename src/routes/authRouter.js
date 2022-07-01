a08481cb302b7890b04875c85ddadbd3c6cd5673
williameiji <william.fujimori@gmail.com>
2022-07-01 17:30:17 -0300

    fix access

--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -7,6 +7,13 @@ import createToken from "../infra/middlewares/createToken.js";
 
 const authRouter = Router();
 
+authRouter.use((req, res, next) => {
+	res.append("Access-Control-Allow-Origin", ["*"]);
+	res.append("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+	res.append("Access-Control-Allow-Headers", "Content-Type");
+	next();
+});
+
 authRouter.post("/signup", signupValidator, addNewUser, signUp);
 authRouter.post("/login", signinValidator, createToken, signIn);
 
--- src/routes/historyRouter.js
+++ src/routes/historyRouter.js
@@ -17,6 +17,13 @@ import editRecordValidator from "../infra/validators/editRecordValidator.js";
 
 const historyRouter = Router();
 
+historyRouter.use((req, res, next) => {
+	res.append("Access-Control-Allow-Origin", ["*"]);
+	res.append("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+	res.append("Access-Control-Allow-Headers", "Content-Type");
+	next();
+});
+
 historyRouter.post(
 	"/history",
 	verifyToken,
